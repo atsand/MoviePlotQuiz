e7507264b9d67be7d0d073db8e1649b124785afd
Andrew Patenge <patenged@gmail.com>
2018-03-06 17:04:29 -0500

    got randomid

--- MoviePlotQuiz/Controllers/HomeController.cs
+++ MoviePlotQuiz/Controllers/HomeController.cs
@@ -55,6 +55,7 @@ namespace MoviePlotQuiz.Controllers
 
         public ActionResult Quiz()
         {
+            GetMovieData(IDs1Controller.RandomId());
             int id = 0;
             ViewBag.id = id;
             id++;
