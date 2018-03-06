b96f37e5eb9b7ebdc4a0467f7f788a07efb21e5a
Aaron <atsand23@gmail.com>
2018-03-06 10:48:04 -0500

    Connected to online DB.  Created MVC based off table.

---
+++ MoviePlotQuiz/Controllers/IDs1Controller.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MoviePlotQuiz.Models;
+
+namespace MoviePlotQuiz.Controllers
+{
+    public class IDs1Controller : Controller
+    {
+        private MoviesEntities1 db = new MoviesEntities1();
+
+        // GET: IDs1
+        public ActionResult Index()
+        {
+            return View(db.IDs.ToList());
+        }
+
+        // GET: IDs1/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ID iD = db.IDs.Find(id);
+            if (iD == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iD);
+        }
+
+        // GET: IDs1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: IDs1/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID1,ImdbId,Title,Genre")] ID iD)
+        {
+            if (ModelState.IsValid)
+            {
+                db.IDs.Add(iD);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(iD);
+        }
+
+        // GET: IDs1/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ID iD = db.IDs.Find(id);
+            if (iD == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iD);
+        }
+
+        // POST: IDs1/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID1,ImdbId,Title,Genre")] ID iD)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iD).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(iD);
+        }
+
+        // GET: IDs1/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ID iD = db.IDs.Find(id);
+            if (iD == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iD);
+        }
+
+        // POST: IDs1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ID iD = db.IDs.Find(id);
+            db.IDs.Remove(iD);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MoviePlotQuiz/Models/MovieModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviePlotQuiz.Models
+{
+    public class MovieModel
+    {
+        public string imdbID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Plot { get; set; }
+
+        public string Director { get; set; }
+
+        public string Actors { get; set; }
+
+        public string Poster { get; set; }
+    }
+}
